bcfe573c4b804b6ece6472b8d4c1ecf1c38b67fa
angelmikeal <nwaokocham@gmail.com>
2022-01-10 22:57:03 +0100

    mobile styling fix 2

--- src/main/navbar/navbar.js
+++ src/main/navbar/navbar.js
@@ -54,8 +54,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.newsPaper.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -64,8 +64,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.emailIcon.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -74,8 +74,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.pinIcon.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -84,8 +84,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.cartIcon.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -94,8 +94,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.shopIcon.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -104,8 +104,8 @@ const Navbar = () => {
                 <img
                   src={icons.navIcons.setProfile.url}
                   alt="drop menu"
-                  width="30px"
-                  height="30px"
+                  width="20px"
+
                 />
               </li>
             </Link>
@@ -113,8 +113,7 @@ const Navbar = () => {
               <img
                 src={icons.navIcons.closeMenu.url}
                 alt="drop menu"
-                width="30px"
-                height="30px"
+                width="20px"
               />
             </li>
           </ul>
