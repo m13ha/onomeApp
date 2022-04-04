87263a4a7401e22f62e9d9f5f596728bc31be9db
angelmikeal <nwaokocham@gmail.com>
2022-04-04 23:39:39 +0100

    login updates and client side socket package

--- client/src/controller.js/login.js
+++ client/src/controller.js/login.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const loginUser = async (data, setErrorMsg) => {
+const loginUser = async (data, setErrorMsg, setUser) => {
   let storage = sessionStorage;
   let success = await axios
     .post("/api/login", data)
     .then((res) => {
-      console.log(res.data);
       storage.setItem(
         "onomeUser",
         JSON.stringify(res.data)
       );
+      setUser(res.data)
       return true;
     })
     .catch((err) => {
